c740830395bd9a0dd732a5eddb7d99297783e1aa
akbaldridge <akbaldridge@gmail.com>
2015-10-26 12:37:13 -0400

    Update for Figure_3.R
    Addresses issue:  fig 3 edits #4
    
    -Added Never as category (and coded all NAs as Nevers)
    -Recoded Monthly to be "Frequent" instead of "Infrequent"
    -Stacked Bars 
    -Removed "other" categories
    -Removed legend

--- Figure_3.R
+++ Figure_3.R
@@ -13,9 +13,10 @@ fig3_data <- survey_data %>% select(starts_with("Q2"),starts_with("Q3"),starts_w
 #### re-code data for 2b ####
 fig3_recoded <- fig3_data %>% gather() %>% separate(key,sep="_",c('question','type','subtype'))
 # lump frequencies
+fig3_recoded$value[is.na(fig3_recoded$value)]<-"Never"  #assumes that NAs (blanks) indicate that the activity was never done
 fig3_recoded[fig3_recoded=="Daily"] <- "Frequently"
 fig3_recoded[fig3_recoded=="Weekly"] <- "Frequently"
-fig3_recoded[fig3_recoded=="Monthly"] <- "Infrequently"
+fig3_recoded[fig3_recoded=="Monthly"] <-  "Frequently"  #changed Monthly to be recoded as "Frequently"
 fig3_recoded[fig3_recoded=="Yearly or less"] <- "Infrequently"
 
 
@@ -30,19 +31,23 @@ for(i in unique(fig3_recoded$type)){
         filter(type==i) %>% 
         group_by(subtype) %>% 
         summarize(mean_Frequently=Frequently/nrow(survey_data)*100,
-                  mean_Infrequently=Infrequently/nrow(survey_data)*100)
+                  mean_Infrequently=Infrequently/nrow(survey_data)*100,
+                  mean_Never=Never/nrow(survey_data)*100) #Added calculation of mean for Never
     
     #re-order
     fig3_summary <- fig3_summary[order(fig3_summary$mean_Infrequently,decreasing = TRUE),]
+    
+    #remove other and otherspecify from subtype list
+    fig3_summary <- fig3_summary[which(fig3_summary$subtype != "other" & fig3_summary$subtype != "otherspecify" &fig3_summary$subtype != "specifyother"), ] #!!
     
     #create labels
     plot_labels <- as.character(fig3_summary$subtype)
     
     #plot
     barplot(t(as.matrix(fig3_summary[,-1])),
-            beside=T,
-            legend = c("Frequently","Infrequently"),
-            args.legend = list(x="top",bty='n'),
+            beside=F,  #changed to F to stack bars
+            #legend = c("Frequently","Infrequently"),   #cancelled out legend to simplify figure and save headache
+            #args.legend = list(x="top",bty='n'),
             names=plot_labels,
             las=3,
             ylim=c(0,100),
