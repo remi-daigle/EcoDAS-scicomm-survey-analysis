0be819a7b6ed956f91a8c1d9294484e7bc010194
Remi Daigle <remi.daigle@dal.ca>
2015-10-26 22:29:16 -0300

    fix fig 4
    fixed #1
    fixed #2

--- Figure_4.R
+++ Figure_4.R
@@ -25,8 +25,17 @@ fig4_summary <- fig4_summary %>%
     spread(subtype,value) %>% 
     filter(type!='other')
 
+# recode type
+unique(fig4_summary$type)
+fig4_summary$type[fig4_summary$type=="oral"]<-"Oral\nn = 24"
+fig4_summary$type[fig4_summary$type=="workshop"]<-"Workshop\nn = 12"
+fig4_summary$type[fig4_summary$type=="popmedia"]<-"Pop\nMedia\nn = 13"
+fig4_summary$type[fig4_summary$type=="written"]<-"Written\nn = 22"
+fig4_summary$type[fig4_summary$type=="visual"]<-"Visual\nn = 12"
+fig4_summary$type[fig4_summary$type=="socialmedia"]<-"Social\nMedia\nn = 11"
+
 #re-scale so that columns are relative to n
-fig4_summary[,-1] <- fig4_summary[,-1]/apply(fig4_summary[,-1],1,sum)*n/nrow(survey_data)*100
+# fig4_summary[,-1] <- fig4_summary[,-1]/apply(fig4_summary[,-1],1,sum)*n/nrow(survey_data)*100
 
 #re-order
 fig4_summary <- fig4_summary[order(apply(fig4_summary[,-1],1,sum),decreasing = TRUE),]
@@ -34,16 +43,27 @@ fig4_summary <- fig4_summary[,c(1,1+order(as.numeric(apply(fig4_summary[,-1],2,s
 
 #create labels
 plot_labels <- as.character(fig4_summary$type)
-legend_labels <- names(fig4_summary[,-1])
+legend_labels <- c("Your Univerisity/Institution",
+                   "Self-guided tutorial/training",
+                   "Other",
+                   "Conference/Scientific Society",
+                   "Another University/Institution",
+                   "Government")
 
 #plot
 jpeg('fig4.jpg')
-barplot(t(as.matrix(fig4_summary[,-1])),
+par(mar=c(6, 4, 0, 0) + 0.1)
+mp <- barplot(t(as.matrix(fig4_summary[,-1])),
         legend = legend_labels,
         args.legend = list(x="topright",bty='n'),
-        names=plot_labels,
-        las=3,
-        ylim=c(0,100),
-        ylab="Percent",
-        col=rainbow(6))
+        ylim=c(0,50),
+        ylab="Total Number of Responses",
+        col=rainbow(6),
+        axes = FALSE,
+        names=rep("",6))
+mtext("Science Communication Category (n = # of individuals)",1,5)
+axis(2)
+axis(1,at=mp,labels=rep("",6))
+axis(1,labels=plot_labels,at=mp,pos=c(-4,0),lwd=0)
+box(bty='l')
 dev.off()
