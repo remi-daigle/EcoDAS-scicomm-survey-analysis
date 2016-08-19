06e92e9ac26f9a60ff3e344b46dbf6e974d3f2d6
Remi Daigle <remi.daigle@dal.ca>
2016-08-19 12:43:51 -0300

    tif'ed fig 4/3

--- Figure_4.R
+++ Figure_4.R
@@ -7,54 +7,54 @@
 
 # get the right data
 require(dplyr)
-fig4_data <- survey_data %>% select(starts_with("Q10"))
+fig3_data <- survey_data %>% dplyr::select(starts_with("Q10"))
 
 # how many respondents received each type of funding (can be multiple subtypes)
 n <- c(24,12,13,22,12,11)
 
 # recode
-fig4_recoded <- fig4_data
-fig4_recoded[is.na(fig4_data)] <- 0
-fig4_recoded[!is.na(fig4_data)] <- 1
+fig3_recoded <- fig3_data
+fig3_recoded[is.na(fig3_data)] <- 0
+fig3_recoded[!is.na(fig3_data)] <- 1
 
 require(tidyr)  #necessary for creating summary
-fig4_summary <- as.data.frame(t(apply(fig4_recoded,2,function(x) sum(as.numeric(x)))))
-fig4_summary <- fig4_summary %>% 
+fig3_summary <- as.data.frame(t(apply(fig3_recoded,2,function(x) sum(as.numeric(x)))))
+fig3_summary <- fig3_summary %>% 
     gather() %>% 
     separate(key,c('question','category','type','subtype')) %>% 
-    select(-question,-category) %>% 
+    dplyr::select(-question,-category) %>% 
     spread(subtype,value) %>% 
     filter(type!='other')
 
 # recode type
-unique(fig4_summary$type)
-fig4_summary$type[fig4_summary$type=="oral"]<-"Oral\nn = 24"
-fig4_summary$type[fig4_summary$type=="workshop"]<-"Workshop\nn = 12"
-fig4_summary$type[fig4_summary$type=="popmedia"]<-"Popular\nMedia\nn = 13" 
-fig4_summary$type[fig4_summary$type=="written"]<-"Written\nn = 22"
-fig4_summary$type[fig4_summary$type=="visual"]<-"Visual\nn = 12"
-fig4_summary$type[fig4_summary$type=="socialmedia"]<-"Social\nMedia\nn = 11"
+unique(fig3_summary$type)
+fig3_summary$type[fig3_summary$type=="oral"]<-"Oral\nn = 24"
+fig3_summary$type[fig3_summary$type=="workshop"]<-"Workshop\nn = 12"
+fig3_summary$type[fig3_summary$type=="popmedia"]<-"Popular\nMedia\nn = 13" 
+fig3_summary$type[fig3_summary$type=="written"]<-"Written\nn = 22"
+fig3_summary$type[fig3_summary$type=="visual"]<-"Visual\nn = 12"
+fig3_summary$type[fig3_summary$type=="socialmedia"]<-"Social\nMedia\nn = 11"
 
 #re-scale so that columns are relative to n
-# fig4_summary[,-1] <- fig4_summary[,-1]/apply(fig4_summary[,-1],1,sum)*n/nrow(survey_data)*100
+# fig3_summary[,-1] <- fig3_summary[,-1]/apply(fig3_summary[,-1],1,sum)*n/nrow(survey_data)*100
 
 #re-order
-fig4_summary <- fig4_summary[order(apply(fig4_summary[,-1],1,sum),decreasing = TRUE),]
-fig4_summary <- fig4_summary[,c(1,1+order(as.numeric(apply(fig4_summary[,-1],2,sum))))]
+fig3_summary <- fig3_summary[order(apply(fig3_summary[,-1],1,sum),decreasing = TRUE),]
+fig3_summary <- fig3_summary[,c(1,1+order(as.numeric(apply(fig3_summary[,-1],2,sum))))]
 
 # do stats on summary format data
-fig4_stats <- fig4_recoded %>% 
+fig3_stats <- fig3_recoded %>% 
     gather() %>% 
     separate(key, c('question','category','type','subtype'), sep="_") %>% 
-    select(-question,-category)
-fig4_stats$value <- factor(fig4_stats$value)
-mylogit <- glm(value ~ type+subtype, data = fig4_stats, family = "binomial")
+    dplyr::select(-question,-category)
+fig3_stats$value <- factor(fig3_stats$value)
+mylogit <- glm(value ~ type+subtype, data = fig3_stats, family = "binomial")
 summary(mylogit)
 confint(mylogit)
 
 
 #create labels
-plot_labels <- as.character(fig4_summary$type)
+plot_labels <- as.character(fig3_summary$type)
 legend_labels <- c("Government",
                    "Another University/Institution",
                    "Conference/Scientific Society",
@@ -63,14 +63,17 @@ legend_labels <- c("Government",
                    "Your Univerisity/Institution")  #revised order
 
 #plot
-jpeg('fig4.jpg')
+require(RColorBrewer)
+display.brewer.all(5,colorblindFriendly = T)
+tiff('fig3.tif',res=1200,width=8.5,height=5,units="in")
 par(mar=c(6, 4, 1, 0) + 0.1)
-mp <- barplot(t(as.matrix(fig4_summary[,-1])),
+mp <- barplot(t(as.matrix(fig3_summary[,-1])),
         legend = legend_labels,
         args.legend = list(x="topright",bty='n'),
         ylim=c(0,50),
         ylab="Total Number of Responses",
-        col=c('#253494','#2c7fb8','#41b6c4','#7fcdbb','#c7e9b4', '#ffffcc'),
+        col=brewer.pal(6,'Set2')[c(1,6,2,3,4,5)],     # the ones below are better suited for sequential quantitative data, (i.e. gradients are not ideal for representing categorical data) 
+        # col=c('#253494','#2c7fb8','#41b6c4','#7fcdbb','#c7e9b4', '#ffffcc'),
 #        col=c('#662506', '#993404','#d95f0e','#fe9929','#fec44f','#ffffd4'),   #alternate color scheme 1: the 70's
 #        col=c('#00441b', '#006d2c','#41ae76','#99d8c9','#ccece6','#f7fcfd'),   #alternate color scheme 2: Green Dream
         axes = FALSE,
@@ -81,3 +84,7 @@ axis(1,at=mp,labels=rep("",6))
 axis(1,labels=plot_labels,at=mp,pos=c(-4,0),lwd=0)
 box(bty='l')
 dev.off()
+
+fig <- image_read("fig3.tif")
+info <- image_info(fig)
+image_write(image_scale(fig,paste0(info$width/5,"X",info$height/5)),"fig3_small.png",format='png')
