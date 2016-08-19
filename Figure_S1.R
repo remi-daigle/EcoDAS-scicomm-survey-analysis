c09995cf431799d0f9d3806dad271ed5178cb6f0
Remi Daigle <remi.daigle@dal.ca>
2016-08-19 14:05:52 -0300

    created fig S1
    from Beth's excel explorations

---
+++ Figure_S1.R
@@ -0,0 +1,59 @@
+#### Figure S1 ####
+# REASONING:
+# - Show intended career trajectories of survey participants
+
+# load packages (install these if you don't have them)
+require(dplyr)
+require(tidyr)
+
+# get the right data
+figS1_data <- data.frame(demo_data %>% dplyr::select(starts_with("Q25")))
+
+
+#### make a bar plot ####
+#re-organize
+figS1_data_long <- figS1_data %>% gather(type,answer)
+figS1_data_long$answer <- as.numeric(is.na(figS1_data_long$answer)==0)
+figS1_summary <-  figS1_data_long %>% 
+    group_by(type) %>% 
+    summarize(counts=sum(answer)) %>% 
+    ungroup()
+
+#re-order to make a better looking plot
+figS1_summary <- figS1_summary[c(6,7,3,9,10,4,8,5,2,1), ]
+figS1_summary
+
+#alternate assign labels- to make labels more self-explanatory
+figS1_summary$type <- c('High School\nteaching',
+                        'Industry',
+                        'Entrepreneurship',
+                        'Other',
+                        'Science\ncommunication',
+                        'Government -\nPolicy',
+                        'Non-governmental\norganization',
+                        'Government -\nResearch',
+                        'Academia -\nTeaching',
+                        'Academia -\nResearch')
+
+# make count numeric
+figS1_summary$counts <- as.numeric(figS1_summary$counts)
+
+#plot
+tiff('figS1.tif',res=1200,width=3.5,height=7,units="in")
+
+par(mar=c(5.1, 8.1, 0.1, 0.6),cex=0.9)
+barplot(figS1_summary$counts,
+        horiz=T,
+        names.arg = figS1_summary$type,
+        las=1,
+        xlim=c(0,20),
+        xlab="Number of Responses",
+        col=c('#99d8c9')
+        )
+box(bty='l')
+
+dev.off()
+
+fig <- image_read("figS1.tif")
+info <- image_info(fig)
+image_write(image_scale(fig,paste0(info$width/5,"X",info$height/5)),"figS1_small.png",format='png')
